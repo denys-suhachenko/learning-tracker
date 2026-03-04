e3a880100f215dcbfee63328219180dc9bdacd9e
Denys <denys.x95@gmail.com>
2026-03-04 18:53:09 +0200

    added ThemeProvider and theme switcher to header

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,16 @@ import { Provider } from 'react-redux';
 import { router } from './app/router/router.tsx';
 import { store } from './app/store/store.ts';
 
+import { ThemeProvider } from './app/providers/ThemeProvider.tsx';
+
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 );
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,15 +1,20 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
+import { ThemeToggle } from './ThemeToggle';
+
 export const Header = () => {
   return (
-    <header className="relative border-b border-gray-300 bg-white shadow-xs">
-      <div className="px-12 py-4.5">
-        <div className="flex items-center gap-x-2 text-gray-500">
-          <MagnifyingGlassIcon className="size-5" />
+    <header className="relative border-b border-gray-300 bg-white shadow-xs dark:bg-gray-900 dark:text-white">
+      <div className="flex items-center justify-between px-12 py-4.5">
+        <div className="flex items-center gap-x-2 text-gray-900 dark:text-white">
+          <MagnifyingGlassIcon className="size-4" />
           <input
             placeholder="Search"
-            className="w-64 py-1 text-sm text-black outline-none"
+            className="w-64 py-1 text-sm text-black outline-none dark:text-white"
           />
+        </div>
+        <div>
+          <ThemeToggle />
         </div>
       </div>
     </header>
