31508b47dada775712c53d27ac0ca4e86a2a910b
Denys <denys.x95@gmail.com>
2026-03-18 02:28:06 +0200

    hotfix: removed old auth selectores

--- src/features/auth/model/selectors.ts
+++ src/features/auth/model/selectors.ts
@@ -2,6 +2,3 @@ import type { RootState } from '@/app/store/store';
 
 export const selectAccessToken = (state: RootState) => state.auth.accessToken;
 export const selectUser = (state: RootState) => state.auth.user;
-export const selectIsAuthenticated = (state: RootState) =>
-  state.auth.isAuthenticated;
-export const selectIsHydrated = (state: RootState) => state.auth.isHydrated;
