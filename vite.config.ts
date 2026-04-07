7e028c480a92fac0d28aef2e31b5fe3935041829
Denys <denys.x95@gmail.com>
2026-04-07 18:09:13 +0300

    added sentry config (#18)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: 'hidden',
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        filesToDeleteAfterUpload: ['./dist/**/*.map'],
+      },
+    }),
+  ],
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    __COMMIT_SHA__: JSON.stringify(
+      process.env.VERCEL_GIT_COMMIT_SHA || 'local',
+    ),
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
