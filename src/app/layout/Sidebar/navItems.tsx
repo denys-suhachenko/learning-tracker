492040edcefc5355f4b477b1dd754d9fc1e19667
Denys <denys.x95@gmail.com>
2026-06-02 15:15:42 +0300

    changed sidebar styles

--- src/app/layout/Sidebar/navItems.tsx
+++ src/app/layout/Sidebar/navItems.tsx
@@ -10,26 +10,26 @@ export const navItems = [
   {
     path: '/',
     label: 'Dashboard',
-    icon: <HouseIcon className="size-5" />,
+    icon: <HouseIcon className="size-6" />,
   },
   {
     path: '/courses',
     label: 'Courses',
-    icon: <BookAIcon className="size-5" />,
+    icon: <BookAIcon className="size-6" />,
   },
   // {
   //   path: '/planner',
   //   label: 'Planner',
-  //   icon: <CalendarIcon className="size-5" />,
+  //   icon: <CalendarIcon className="size-6" />,
   // },
   {
     path: '/review',
     label: 'Review',
-    icon: <RefreshCcwIcon className="size-5" />,
+    icon: <RefreshCcwIcon className="size-6" />,
   },
   // {
   //   path: '/knowledge-base',
   //   label: 'Knowledge Base',
-  //   icon: <LibraryBigIcon className="size-5" />,
+  //   icon: <LibraryBigIcon className="size-6" />,
   // },
 ];
